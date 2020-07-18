da55bac9af874f34816cd5ddd09789a851c6e36a
Tiffani Best <tiffanimelissabest@gmail.com>
2020-07-18 21:32:55 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,1 @@
 def display_board
-board = [" "," "," "," "," "," "," "," "," "]
-expect(rows[0]).to eq("   |   |   ")
-      expect(rows[1]).to eq("-----------")
-      expect(rows[2]).to eq("   |   |   ")
-      expect(rows[3]).to eq("-----------")
-      expect(rows[4]).to eq("   |   |   ")
-    end
