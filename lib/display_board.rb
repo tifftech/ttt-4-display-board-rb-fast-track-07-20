22f65ae058d769753d752535894d3c62db7f433e
Tiffani Best <tiffanimelissabest@gmail.com>
2020-07-18 21:28:02 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,4 +1,7 @@
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def display_board(board)
-  
+  puts " # | # | # "
+  puts "-----------"
+  puts " # | # | # "
+  puts "-----------"
+  puts " # | # | # "
 end
